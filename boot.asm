ded59f1f52e27c8921535800f6133959f55461d4
Max Dowling <maxdowling1764@gmail.com>
2022-11-26 13:51:06 -0600

    Added disk load

--- boot.asm
+++ boot.asm
@@ -1,19 +1,12 @@
 [org 0x7c00]
 
-mov bx, msg
-call print_str
-call print_new_line
-
-mov ax, 0xFF42
-mov cx, 0xAABC
-mov bx, 0xAABB
-call coredump
-
 jmp $
 
 %include "print_str.asm"
 %include "print_hex.asm"
 %include "debug.asm"
+%include "disk.asm"
+
 msg:
     db "Starting OS", 0
 
@@ -22,5 +15,14 @@ msg_core_dump:
 label_ax:
     db "disibpspbxdxcxax", 0
 
+DISK_ERR_MSG:
+    db "Error reading disk", 0
+
+BOOT_DRIVE: db 0
+
 times 510 - ($ - $$) db 0
 dw 0xaa55
+
+times 256 dw 0xDADA
+times 256 dw 0xDEAD
+times 1024 dw 0x0000
---
+++ disk.asm
@@ -0,0 +1,23 @@
+disk_load:
+    push dx
+
+    mov ah, 0x02
+    mov al, dh
+    mov ch, 0x00
+    mov dh, 0x00
+    mov cl, 0x02
+
+    int 0x13
+
+    jc disk_err
+
+    pop dx
+    cmp dh, al
+    jne disk_err
+    
+    ret
+
+disk_err:
+    mov bx, DISK_ERR_MSG
+    call print_str
+    jmp $
