942fae2b87e48041af76636dfbf83196879f85bd
Max Dowling <MD057682@Oracle.com>
2022-12-03 08:25:56 -0600

    Fixed missing return value in inportb

--- src/kernel/common.c
+++ src/kernel/common.c
@@ -7,5 +7,7 @@ void outportb(unsigned short port, unsigned char data)
 
 unsigned char inportb(unsigned short port)
 {
+    unsigned char ret;
     __asm__ __volatile__ ("inb %1, %0": "=a" (ret): "dN" (port));
+    return ret;
 }
