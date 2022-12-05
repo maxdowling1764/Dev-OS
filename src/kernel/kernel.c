80b170debb6268f9b04121a075f576768af8fda9
Max Dowling <MD057682@Oracle.com>
2022-12-05 17:18:19 -0600

    Ttesting weird bug with structs

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,6 +2,7 @@
 #include "idt.h"
 #include "isr.h"
 #include "irq.h"
+#include "gdt.h"
 #include "common.h"
 
 static unsigned char keymap[128];
@@ -80,6 +81,14 @@ void main()
     init_irqs();
     set_irq_handler(1, keyboard_handler);
     
+    t_gdt_desc d = *((t_gdt_desc*) GDTPTR_OFFSET);
+
+    typedef struct test 
+    {
+        char c;
+    } t_test;
+
+    *(video_mem) = 'c';
     for(;;)
     {
     } 
